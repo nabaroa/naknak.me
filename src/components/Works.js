7312aee0167adf418a483bf288eb30e3212c82a0
Naiara Abaroa <naiaraabaroa@gmail.com>
2019-05-06 13:46:08 +0100

    Add assets

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,9 +1,254 @@
 import React from 'react';
 
 const Works = () => (
-  <section id="works" className="nak-section">
-    <h1>Works</h1>
-  </section>
+    <section id="works" className="nak-section">
+        <h1>Works</h1>
+        <div>
+            <div className="box col4">
+                <div className="cooking">&nbsp;</div>
+                <img alt="" src="../assets/thegoldrush-reloaded.jpg" />
+                <h2 className="abel contrast">The gold rush. Realoaded</h2>
+                <p className="data">2019</p>
+            </div>
+            <div className="box col4">
+                <img alt="" src="../assets/izan-bagara.jpg" />
+                <h2 className="abel contrast" lang="eu">Izan bagara. Emakumea informatikan</h2>
+                <p className="data">2019</p>
+            </div>
+            <div className="box col4">
+                <img alt="" src="../assets/cover-animate.gif" />
+                <h2 className="abel contrast">Web animation: The <span>new</span> generation</h2>
+                <p className="data">2018</p>
+            </div>
+            <div className="box col4">
+                <img alt="" src="../assets/grid-layout.jpg" />
+                <h2 className="abel contrast">CSS Grid Layout: En busca del fuego</h2>
+                <p></p>
+                <p className="data">2017</p>
+            </div>
+            <div className="box col4">
+                <a className="has-tooltip" href="https://randommatches.live" target="_blank" rel="noopener noreferrer">
+                    <img alt="" src="../assets/randomMatches.jpg" />
+                </a>
+                <h2 className="abel contrast"><a href="https://randommatches.live">#randomMatches</a></h2>
+                <p><span className="firm">#randomMatches</span> is just a game, a way to express myself through images. An
+                    image compilation collected randomly from <a target="_blank" rel="noopener noreferrer" href="https://es.pinterest.com/nabaroa/">Pinterest</a> and published on
+                    <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/nabaroa">Twitter</a>
+                </p>
+                <p className="data">2017</p>
+            </div>
+            <div className="box col4">
+                <a href="https://nabaroa.github.io/erase-una-vez-el-design-system/"><img alt="" src="../assets/design-systems.jpg" /></a>
+                <h2 className="abel contrast"><a href="https://nabaroa.github.io/erase-una-vez-el-design-system/">Erase
+                      una vez... el Design System</a></h2>
+                <p>Web slides on <span className="firm">Design Systems</span> presented at <a className="firm" href="https://bilbostack.com" target="_blank" rel="noopener noreferrer">Bilbostack 2017</a>
+                </p>
+                <p className="data">2017</p>
+            </div>
+            <div className="box col4">
+                <a href="https://nabaroa.github.io/postcss-beyond-treasure-map/"><img alt="" src="../assets/postcss-beyond.jpg" /></a>
+                <h2 className="abel contrast"><a href="https://nabaroa.github.io/postcss-beyond-treasure-map/">PostCSS.
+                      Beyond the treasure map</a></h2>
+                <p>Web slides on <span className="firm">PostCSS</span> presented at <a className="firm" href="https://2016.codemotion.es/agenda.html#5732408326356992/89254005" target="_blank" rel="noopener noreferrer">Codemotion
+                      2016</a>
+                </p>
+                <p className="data">2016</p>
+            </div>
+            <div className="box col4">
+                <a href="https://wimi5.com/"><img alt="" src="../assets/wimi5.jpg" /></a>
+                <h2 className="abel contrast"><a href="https://wimi5.com/">WiMi5</a></h2>
+                <p>I worked for two years and a half in this startup where we developed a cloud based editor to
+                    create, publish and monetize <span className="firm"> HTML5 games</span>
+                </p>
+                <p>My task in this project was to <span className="firm">create and develop the UX/UI</span>
+                </p>
+                <p className="data">2016</p>
+                <p></p>
+            </div>
+            <div className="box col4">
+                <a href="https://nabaroa.github.io/postcss-treasure-map/#/"><img alt="" src="../assets/postcss.jpg" /></a>
+                <h2 className="abel contrast"><a href="https://nabaroa.github.io/postcss-treasure-map/#/">PostCSS. The
+                      treasure map</a></h2>
+                <p>Web slides on <span className="firm">PostCSS</span>
+                </p>
+                <p>
+                </p><p className="data">2016</p>
+            </div>
+            <div className="box col4">
+                <a href="https://nabaroa.github.io/IveSeenThings"><img alt="" src="../assets/iveseenthings.jpg" /></a>
+                <h2 className="abel contrast"><a href="https://nabaroa.github.io/IveSeenThings">I've seen things. CSS
+                      upcoming awesome features</a></h2>
+                <p>Web slides on <span className="firm">CSS new features</span>
+                </p>
+                <p>
+                </p><p className="data">2016</p>
+            </div>
+            <div className="box col4">
+                <a href="https://nabaroa.github.io/theGoldRush/#/"><img alt="" src="../assets/thegoldrush.jpg" /></a>
+                <h2 className="abel contrast"><a href="https://nabaroa.github.io/theGoldRush/#/">The Gold rush. CSS gold
+                      nuggets</a></h2>
+                <p>Web slides on <span className="firm">CSS new features</span>
+                </p>
+                <p>
+                </p><p className="data">2016</p>
+            </div>
+            <div className="box col4">
+                <a href="https://learncss.info/"><img alt="" src="../assets/learnCSS.jpg" /></a>
+                <h2 className="abel contrast"><a href="https://learncss.info/">learnCSS.info</a></h2>
+                <p>Online study material about <span className="firm">CSS</span>
+                </p>
+                <p className="data">2015</p>
+            </div>
+            <div className="box col4">
+                <a href="https://areyousassy.zz.mu/"><img alt="" src="../assets/areyousassy_cover.jpg" /></a>
+                <h2 className="abel contrast"><a href="https://areyousassy.zz.mu/">Are you sassy?</a></h2>
+                <p>Web slides on <span className="firm">Sass</span> at <a href="https://devfestbilbao.org/" target="_blank" rel="noopener noreferrer">DevFestBilbao</a> organized by <a href="https://twitter.com/gdgbilbao" target="_blank" rel="noopener noreferrer">GDG Bilbao</a>.
+                  </p>
+                <p className="data">2014</p>
+                <p></p>
+            </div>
+            <div className="box col4">
+                <img alt="" src="../assets/especificacion.png" />
+                <h2 className="abel contrast" lang="es">La especificación CSS3</h2>
+                <p>Web slides for<span className="firm"> CSS3 </span>clases at <a href="https://www.postgrado.deusto.es/cs/Satellite/estudiantes/es/buscador-de-postgrados/diploma-de-especializacion-en-soluciones-software-para-internet-/programa" target="_blank" rel="noopener noreferrer">#dessi @deusto</a>
+                </p>
+                <p className="data">2014</p>
+                <p><a href="https://github.com/nabaroa/naknak.me/blob/master/docs/pdf/EspecificacionCSS3.pdf" target="_blank" rel="noopener noreferrer">Go to slides</a>
+                </p>
+            </div>
+            <div className="box col4">
+                <img alt="" src="../assets/animacion.png" />
+                <h2 className="abel contrast" lang="es">Animación y transición CSS3</h2>
+                <p>Web slides for <span className="firm">CSS3</span> clases at <a href="https://www.postgrado.deusto.es/cs/Satellite/estudiantes/es/buscador-de-postgrados/diploma-de-especializacion-en-soluciones-software-para-internet-/programa" target="_blank" rel="noopener noreferrer">#dessi @deusto</a>
+                </p>
+                <p className="data">2014</p>
+                <p><a href="https://github.com/nabaroa/naknak.me/blob/master/docs/pdf/Animation_transitionCSS.pdf" target="_blank" rel="noopener noreferrer">Go to slides</a>
+                </p>
+            </div>
+            <div className="box col4">
+                <img alt="" src="../assets/flexbox.jpg" />
+                <h2 className="abel contrast">Flexbox</h2>
+                <p>Web slides for <span className="firm">CSS3</span> clases at <a href="https://www.postgrado.deusto.es/cs/Satellite/estudiantes/es/buscador-de-postgrados/diploma-de-especializacion-en-soluciones-software-para-internet-/programa" target="_blank" rel="noopener noreferrer">#dessi @deusto</a>
+                </p>
+                <p className="data">2014</p>
+                <p><a href="https://github.com/nabaroa/naknak.me/blob/master/docs/pdf/Flexbox.pdf" target="_blank" rel="noopener noreferrer">Go
+                      to slides</a>
+                </p>
+            </div>
+            <div className="box col4">
+                <img alt="" src="../assets/rwd.jpg" />
+                <h2 className="abel contrast">Responsive Web Design</h2>
+                <p>Web slides for <span className="firm">CSS3</span> clases at <a href="https://www.postgrado.deusto.es/cs/Satellite/estudiantes/es/buscador-de-postgrados/diploma-de-especializacion-en-soluciones-software-para-internet-/programa" target="_blank" rel="noopener noreferrer">#dessi @deusto</a>
+                </p>
+                <p className="data">2014</p>
+                <p><a href="https://fliiby.com/file/v3e1u2o2h0c/" target="_blank" rel="noopener noreferrer">Go to slides</a>
+                </p>
+            </div>
+            <div className="box col4">
+                <img alt="" src="../assets/selectors.jpg" />
+                <h2 className="abel contrast">CSS selectors</h2>
+                <p>Web slides for <span className="firm">CSS3</span> clases at <a href="https://www.postgrado.deusto.es/cs/Satellite/estudiantes/es/buscador-de-postgrados/diploma-de-especializacion-en-soluciones-software-para-internet-/programa" target="_blank" rel="noopener noreferrer">#dessi @deusto</a>
+                </p>
+                <p className="data">2014</p>
+                <p><a href="https://github.com/nabaroa/naknak.me/blob/master/docs/pdf/CSSselectors.pdf" target="_blank" rel="noopener noreferrer">Go to slides</a>
+                </p>
+            </div>
+            <div className="box col4">
+                <img alt="" src="../assets/sass.jpg" />
+                <h2 className="abel contrast">Sass</h2>
+                <p>Web slides for <span className="firm">CSS3</span> clases at <a href="https://www.postgrado.deusto.es/cs/Satellite/estudiantes/es/buscador-de-postgrados/diploma-de-especializacion-en-soluciones-software-para-internet-/programa" target="_blank" rel="noopener noreferrer">#dessi @deusto</a>
+                </p>
+                <p className="data">2014</p>
+                <p><a href="https://github.com/nabaroa/naknak.me/blob/master/docs/pdf/Sass.pdf" target="_blank" rel="noopener noreferrer">Go to
+                      slides</a>
+                </p>
+            </div>
+            <div className="box col4">
+                <img alt="" src="../assets/sassgirls.jpg" />
+                <h2 className="abel contrast">FrontGirls</h2>
+                <p>Study team to improve our <span className="firm">front-end</span> skills, focused on having more
+                  superpowers.
+                  </p>
+                <p className="data">2014</p>
+                <p><a href="https://frontgirls.io" target="_blank" rel="noopener noreferrer">Go to site</a>
+                </p>
+            </div>
+            <div className="box col4">
+                <img alt="" src="../assets/theantievent.jpg" />
+                <h2 className="abel contrast">theAntiEvent 2014</h2>
+                <p>Web site of the <a href="https://theantievent.org/" target="_blank" rel="noopener noreferrer">Event</a>
+                </p>
+                <p className="data">2014</p>
+                <p><a href="https://theantievent.org/" target="_blank" rel="noopener noreferrer">Go to site</a>
+                </p>
+            </div>
+            <div className="box col4">
+                <img alt="" src="https://raw.githubusercontent.com/nabaroa/naknak.me/master/docs/assets/concept.jpg" />
+                <h2 className="abel contrast">Practice safe design, use a concept</h2>
+                <p>Web slides for the online class of <a href="https://escuela.it/cursos/curso-gratuito-de-iniciacion-al-desarrollo-web/" target="_blank" rel="noopener noreferrer">Escuela IT</a>
+                </p>
+                <p className="data">2013</p>
+                <p><a href="https://www.youtube.com/watch?v=LCBf0qh6LQI" target="_blank" rel="noopener noreferrer">Go to talk</a>
+                </p>
+            </div>
+            <div className="box col4">
+                <img alt="" src="https://images.indiegogo.com/medias/958015/primary_pictures/full/20130731151522-anticamp_crowdfunding.jpg?1375308926" />
+                <h2 className="abel contrast">TheAntiCamp</h2>
+                <p className="data">2013</p>
+                <p><a href="https://naknak.me/theanticamp" target="_blank" rel="noopener noreferrer">Go to site</a>
+                </p>
+            </div>
+            <div className="box col4">
+                <img alt="" src="../assets/cross.jpg" />
+                <h2 className="abel contrast">
+                    &lt;3 the cross browser</h2> <p>Web slides</p>
+                <p className="data">2013</p>
+                <p>Project created for <a href="https://www.html5-spain.com/html5-spain/fridaywebforward/landing.html" target="_blank" rel="noopener noreferrer" className="firm">#FridayWebFordward</a>
+                </p>
+                <p><a href="https://www.youtube.com/watch?v=WLNDykQpI9A" target="_blank" rel="noopener noreferrer">Go to talk</a>
+                </p>
+            </div>
+            <div className="box col4">
+                <img alt="" src="../assets/placecolor.jpg" />
+                <h2 className="abel contrast">placeColor.in</h2>
+                <p>Web: Design and front-end development</p>
+                <p className="data">2013</p>
+                <p>Project created in the <a href="https://hackmite.elcomite.org/" target="_blank" rel="noopener noreferrer" className="firm">#hackmite</a>
+                </p>
+                <p></p>
+            </div>
+            <div className="box col4">
+                <img alt="" src="../assets/css.jpg" />
+                <h2 className="abel contrast">The CSS Preprocessor</h2>
+                <p>Web slides for the speech in Bilbostack </p>
+                <p className="data">2013</p>
+                <p><a href="https://www.youtube.com/watch?v=pDTmukK6kAY" target="_blank" rel="noopener noreferrer">Go to site</a>
+                </p>
+            </div>
+            <div className="box col4">
+                <img alt="" src="../assets/emmet.jpg" />
+                <h2 className="abel contrast">Emmet.io Improve your HTML &amp; CSS workflow</h2>
+                <p>Web slides</p>
+                <p className="data">2012</p>
+                <p><a href="https://naknak.me/emmet " target="_blank" rel="noopener noreferrer">Go to site</a>
+                </p>
+            </div>
+            <div className="box col4">
+                <img alt="" src="../assets/zen.jpg" />
+                <h2 className="abel contrast">Zen coding</h2>
+                <p>Web slides</p>
+                <p className="data">2012</p>
+                <p><a href="https://naknak.me/zencoding " target="_blank" rel="noopener noreferrer">Go to site</a>
+                </p>
+            </div>
+            <div className="box col4">
+                <img alt="" src="../assets/nak_papel.jpg" />
+                <h2 className="abel contrast">Corporate image #nak</h2>
+                <p>Cards, steakers,...</p>
+                <p className="data">2012</p>
+            </div>
+        </div>
+    </section>
 );
 
 export default Works;
