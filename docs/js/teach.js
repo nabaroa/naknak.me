11d41331ba2074177b0071e5a93719577202230f
Naiara Abaroa <naiaraabaroa@gmail.com>
2022-02-17 21:44:15 +0100

    Json for agenda

---
+++ docs/js/teach.js
@@ -0,0 +1,181 @@
+const events = [
+  {
+    id: "1",
+    date: "28/10/2016-12/11/2016",
+    where: "Bilbao (Basque Country)",
+    company: "Mondragon Unibertsitatea",
+    companyUrl:
+      "https://www.mondragon.edu/es/estudios/master/master-universitario-en-marketing-digital/",
+    what: "Workshop",
+    title: "HTML/CSS",
+    assetLink: "https://escuela.it/cursos/postcss-mas-alla-del-css/",
+  },
+  {
+    id: "2",
+    date: "18/04/2016-22/04/2016",
+    where: "",
+    company: "Escuela IT",
+    companyUrl: "https://escuela.it/",
+    what: "Workshop online",
+    title: "PostCSS",
+    assetLink: "https://escuela.it/cursos/postcss-mas-alla-del-css/",
+    assetLink2: "https://github.com/EscuelaIt/PostCSS-2016",
+  },
+  {
+    id: "3",
+    date: "13/10/2015-16/10/2015",
+    where: "Bilbao (Basque Country)",
+    company: "Universidad de Deusto",
+    companyUrl:
+      "https://www.postgrado.deusto.es/cs/Satellite/estudiantes/es/buscador-de-postgrados/diploma-de-especializacion-en-soluciones-software-para-internet-/programa",
+    what: "Diploma de Especialización de Soluciones Software para Internet",
+    title: "CSS3 module",
+    assetLink: "https://learncss.info/index.html",
+  },
+  {
+    id: "4",
+    date: "24/06/2015",
+    where: "",
+    company: "Escuela IT",
+    companyUrl: "https://escuela.it/",
+    what: "Workshop online",
+    title: "Especificación CSS3 y mejoras en la maquetación con CSS3",
+    assetLink: "https://github.com/EscuelaIt/HTML-CSS-2015/tree/master/CSS3",
+  },
+  {
+    id: "5",
+    date: "05/12/2014 & 12/12/2014",
+    where: "Bilbao (Basque Country)",
+    company: "Coderdojo Bio",
+    companyUrl: "https://coderdojo.com/",
+    what: "Workshop for young people",
+    title: "HTML5",
+  },
+  {
+    id: "6",
+    date: "30/09/2014-3/10/2014",
+    where: "Bilbao (Basque Country)",
+    company: "Universidad de Deusto",
+    companyUrl: "https://www.deusto.es",
+    what: "Diploma de Especialización de Soluciones Software para Internet",
+    title: "CSS3 module",
+  },
+  {
+    id: "7",
+    date: "27/09/2013-1/10/2013",
+    where: "Bilbao (Basque Country)",
+    company: "Universidad de Deusto",
+    companyUrl: "https://www.deusto.es",
+    what: "Diploma de Especialización de Soluciones Software para Internet",
+    title: "CSS3 module",
+  },
+  {
+    id: "8",
+    date: "06/04/2013",
+    where: "",
+    company: "Escuela IT",
+    companyUrl: "https://www.escuela.it/cursos",
+    what: "Workshop online",
+    title: "Taller de preprocesadores CSS: Sass",
+  },
+  {
+    id: "9",
+    date: "nnnnn06/04/2013",
+    where: "",
+    company: "Escuela IT",
+    companyUrl: "https://www.escuela.it/cursos",
+    what: "Workshop online",
+    title: "Taller de preprocesadores CSS: Sass",
+    assetLink: "asdf",
+    assetSlides: "asdfasd",
+    assetVideo: "",
+    assetGithub: "",
+  },
+];
+
+const eventsDiv = document.querySelector(".teach");
+
+events.forEach((event) => {
+  const item = document.createElement("div");
+  item.className = `nk-timeline__item`;
+
+  const title = document.createElement("h3");
+  title.className = "nk-timeline__title";
+  title.innerText = event.title;
+
+  const what = document.createElement("p");
+  what.className = "nk-timeline__description";
+  what.innerText = event.what;
+
+  const company = document.createElement("p");
+  company.innerText = event.company;
+
+  const date = document.createElement("p");
+  date.className = "nk-timeline__date";
+  date.innerText = event.date;
+
+  const where = document.createElement("p");
+  where.className = "nk-timeline__date";
+  where.innerText = event.where;
+
+  const assetLink = document.createElement("a");
+  assetLink.href = event.assetLink;
+
+  const imageLink = document.createElement("img");
+  imageLink.className = "nk-timeline__icon";
+  imageLink.src = "assets/icon-link.svg";
+  imageLink.alt = "Link to documentation";
+
+  const assetSlides  = document.createElement("a");
+  assetSlides.href = event.assetSlides ;
+
+  const imageSlides  = document.createElement("img");
+  imageSlides.className = "nk-timeline__icon";
+  imageSlides.src = "assets/icon-slides.svg";
+  imageSlides.alt = "Slides";
+
+  const assetGithub  = document.createElement("a");
+  assetGithub.href = event.assetGithub ;
+
+  const imageGithub  = document.createElement("img");
+  imageGithub.className = "nk-timeline__icon";
+  imageGithub.src = "assets/icon-github.svg";
+  imageGithub.alt = "Github";
+
+  const assetVideo  = document.createElement("a");
+  assetVideo.href = event.assetVideo ;
+
+  const imageVideo  = document.createElement("img");
+  imageVideo.className = "nk-timeline__icon";
+  imageVideo.src = "assets/icon-video.svg";
+  imageVideo.alt = "Video";
+
+  item.appendChild(title);
+  item.appendChild(what);
+  item.appendChild(company);
+  item.appendChild(date);
+  item.appendChild(where);
+
+  isAssetLinkInObject = "assetLink" in event;
+  isAssetLinkInObject && item.appendChild(assetLink);
+
+  assetLink && assetLink.appendChild(imageLink);
+
+  isAssetSlidesInObject = "assetSlides" in event;
+  isAssetSlidesInObject && item.appendChild(assetSlides);
+
+  assetSlides && assetSlides.appendChild(imageSlides);
+
+  isAssetGithubInObject = "assetGithub" in event;
+  isAssetGithubInObject && item.appendChild(assetGithub);
+
+  assetGithub && assetGithub.appendChild(imageGithub);
+
+  isAssetVideoInObject = "assetVideo" in event;
+  isAssetVideoInObject && item.appendChild(assetVideo);
+
+  assetVideo && assetVideo.appendChild(imageVideo);
+
+
+  eventsDiv.insertBefore(item, eventsDiv.firstChild);
+});
