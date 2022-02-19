8170d696ade76d324dd0d783c4bfb5d6d4744a10
Naiara Abaroa <naiaraabaroa@gmail.com>
2022-02-19 18:31:40 +0100

    Remove jquery

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,9 +1,8 @@
+// Navigation button
 const buttonNavOpen = document.querySelector(".js-menu-toggle");
 const navOpen = document.querySelector(".js-menu");
 const closeByClick = document.getElementsByClassName(".menu__item");
 
-
 buttonNavOpen.onclick = () => navOpen.classList.toggle("is-open");
 
-// closeByClick.onclick = () => navOpen.classList.toggle("is-open");
 closeByClick.onclick = () => console.log('nak');
